34f2b37b53d797c6c7ffb8a19379215dfdc24b2e
Devanandh <100019332+veligaram@users.noreply.github.com>
2024-02-29 09:59:57 +0530

    Time: 221 ms (68.93%) | Memory: 37.9 MB (56.17%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        queue=deque()
+        queue.append(root)
+        level=-1
+        while queue:
+            l=len(queue)
+            level+=1
+            li=[]
+            for _ in range(l):
+                pop=queue.popleft()
+                if level%2==0:
+                    if pop.val%2==0:
+                        return False
+                    else:
+                        if len(li)>0 and li[-1]>=pop.val:
+                            return False
+                        else:
+                            li.append(pop.val)
+                else:
+                    if pop.val%2!=0:
+                        return False
+                    else:
+                        if len(li)>0 and li[-1]<=pop.val:
+                            return False
+                        else:
+                            li.append(pop.val)
+                if pop.left:
+                    queue.append(pop.left)
+                if pop.right:
+                    queue.append(pop.right)
+        return True
+            
+                    
+
+        
