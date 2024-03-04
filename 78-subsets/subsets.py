1dfd7d775086aeeade3e2bc13257f8be15d594fe
Devanandh <100019332+veligaram@users.noreply.github.com>
2024-03-04 19:39:32 +0530

    Time: 33 ms (82.02%) | Memory: 16.7 MB (49.53%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,6 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        li=[[]]
-        for i in nums:
-            li+=[j+[i] for j in li]
-        return li    
+
+        
+
+        def dfs(subset, curr, nums, start):
+            subset.add(tuple(curr))
+            for i in range(start,len(nums)):
+                curr.append(nums[i]) 
+                dfs(subset,curr,nums,i+1)
+                curr.pop(-1)
+
+        subset = set()
+
+        dfs(subset, [], nums, 0)
+
+        return subset
+
+
