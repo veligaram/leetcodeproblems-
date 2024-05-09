5d24380fb09d967f4ee685989a050d8a38db0b12
Devanandh <100019332+veligaram@users.noreply.github.com>
2024-05-09 08:57:39 +0530

    Time: 765 ms (97.78%) | Memory: 43.5 MB (33.89%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -1,13 +1,17 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        happiness.sort(reverse=True)
-        i = 0
-        res = 0
+        children = sorted(happiness, reverse=True)
 
-        while k > 0:
-            happiness[i] = max(happiness[i] - i, 0)
-            res += happiness[i]
-            i += 1
-            k -= 1
+        # Fast calc if no child hits zero happiness
+        # Could do binary search to see if I can do half, quarter, etc this way
+        if children[k-1] >= k-1:
+            return sum(children[:k]) - ((0 + k-1) * k // 2)
+
+        # Else slow way
+        res = 0
+        for i, h in enumerate(children[:k]):
+            if h - i <= 0:
+                break
+            res += h - i
 
         return res
