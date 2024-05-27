263775a58614743949016d9b9eaa3957550aece6
Devanandh <100019332+veligaram@users.noreply.github.com>
2024-05-27 11:06:32 +0530

    Time: 35 ms (85.48%) | Memory: 16.6 MB (14.52%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,11 @@
+import bisect
+
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums=sorted(nums)
+        n=len(nums)
+        for i in range(n,0,-1):
+            k=bisect.bisect_left(nums,i)
+            if(n-k==i):
+                return i
+        return -1
