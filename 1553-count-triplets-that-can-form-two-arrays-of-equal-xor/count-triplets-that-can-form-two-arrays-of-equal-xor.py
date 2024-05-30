635886a1407b4f56ac2af40dca898096cbf705be
Devanandh <100019332+veligaram@users.noreply.github.com>
2024-05-30 15:24:41 +0530

    Time: 37 ms (90.80%) | Memory: 16.4 MB (93.68%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,18 +1,13 @@
 class Solution:
-    def countTriplets(self, arr: list[int]) -> int:
-        res = 0
-
-        for start in range(len(arr) - 1):
-            xor_first_arr = 0  # from start XOR for every subarray is 0
-
-            for second_start in range(start + 1, len(arr)):
-                xor_first_arr ^= arr[second_start - 1]  # count last character to previous XOR
-
-                xor_second_arr = 0  # from start XOR for every subarray is 0
-                for end in range(second_start, len(arr)):
-                    xor_second_arr ^= arr[end]  # count character before end to previous XOR
-
-                    if xor_first_arr == xor_second_arr:  # if XORes are equal
-                        res += 1
-
+    def countTriplets(self, arr: List[int]) -> int:
+        # https://www.youtube.com/watch?v=DwvNIVLcT8U
+        res, hashmap, cur = 0, defaultdict(list), 0
+        for i, ele in enumerate(arr):
+            cur ^= ele
+            if cur == 0:
+                res += i
+            if cur in hashmap:
+                for after in hashmap[cur]:
+                    res += (i - after - 1)
+            hashmap[cur] += [i]
         return res
