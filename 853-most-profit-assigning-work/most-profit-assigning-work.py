6fb0ef492c55b56f70ff6910651723633bbc4af2
Devanandh <100019332+veligaram@users.noreply.github.com>
2024-06-18 11:12:58 +0530

    Time: 408 ms (13.15%) | Memory: 19.6 MB (74.98%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -1,25 +1,13 @@
 class Solution:
-    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        infos = sorted(zip(difficulty, profit))
-        max_profit_for_difficulty = []
-        max_profit = 0
-        for d, p in infos:
-            max_profit = max(max_profit, p)
-            max_profit_for_difficulty.append((d, max_profit))
-        
-        total_profit = 0
+    def maxProfitAssignment(self, d: List[int], profit: List[int], worker: List[int]) -> int:
+        jobs = sorted(zip(d, profit))
+        worker.sort()
+
+        ans = j = maxp = 0
+
         for w in worker:
-            index = self.binary_search(max_profit_for_difficulty, w)
-            if index >= 0 and w >= max_profit_for_difficulty[index][0]:
-                total_profit += max_profit_for_difficulty[index][1]
-        return total_profit
-    
-    def binary_search(self, max_profit_for_difficulty: List[Tuple[int, int]], target: int) -> int:
-        left, right = 0, len(max_profit_for_difficulty) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if max_profit_for_difficulty[mid][0] <= target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return right
+            while j < len(jobs) and jobs[j][0] <= w:
+                maxp = max(maxp, jobs[j][1])
+                j+=1
+            ans += maxp
+        return ans
