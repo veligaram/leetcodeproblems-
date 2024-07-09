cc84787787dfbb4e6c5a8cc842f93aafee4f2c8f
Devanandh <100019332+veligaram@users.noreply.github.com>
2024-07-09 22:19:58 +0530

    Time: 776 ms (19.59%) | Memory: 57.1 MB (45.36%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.py
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -1,15 +1,9 @@
 class Solution:
-    def averageWaitingTime(self, cust: List[List[int]]) -> float:
-        n = len(cust)
-        wait = 0
-        e= -maxsize
-        for val in cust:
-            arrive , time = val
-            if arrive < e: # no one shows up at -9gazillion
-            # so we use this to determine if this is their first time or not
-                wait +=e -arrive +time
-                e+= time
-            else:
-                wait += time
-                e = arrive + time
-        return wait/n
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        available_at = 0
+        total_wait = 0
+        for arrival, t in customers:
+            available_at = max(available_at, arrival) + t
+            total_wait += available_at - arrival
+        
+        return total_wait / len(customers)
