a32db140b5a4cc4aaf85cb054eb01b4a0fe10fee
Devanandh <100019332+veligaram@users.noreply.github.com>
2024-07-10 11:43:52 +0530

    Time: 53 ms (31.31%) | Memory: 16.8 MB (36.19%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.py
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -1,11 +1,12 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        depth = 0
-        for log in logs:
-            if log == "../":
-                depth -= 1 if depth > 0 else 0
-            elif log == "./":
-                depth += 0
+        
+        move = 0
+        for operation in logs:
+            if operation == "../":
+                move -= 1 if move > 0 else 0
+            elif operation == "./":
+                pass
             else:
-                depth += 1
-        return depth
+                move += 1
+        return move
